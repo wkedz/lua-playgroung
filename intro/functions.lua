c19b1d0faa4115c55f1e6a162e245e6cf3cacff0
Wiesław Kędzierski <w.m.kedzierski@gmail.com>
2024-06-16 17:20:11 +0200

    Add intro .

--- intro/functions.lua
+++ intro/functions.lua
@@ -1,9 +1,10 @@
 -- Functions
 --
-function add ( a, b)
+local c , d = 1,2
+local function add ( a, b)
     return a+b
 end
 print("add ", add(2,2))
 
-function add2 (a, b) return a+b end
+local function add2 (a, b) return a+b end
 print("add2 ", add2(1,2))
